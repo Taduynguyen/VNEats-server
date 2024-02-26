c679c2af8747542a4f7c3cc5cd6c9fec68d71223
Taduynguyen <taduynguyen11@gmail.com>
2024-02-26 09:09:42 +0700

    Update-User-Profile-API

--- src/Routes/UserRoute.ts
+++ src/Routes/UserRoute.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import UserController from '../controllers/UserController';
-import { jwtCheck } from '../middleware/auth';
+import { jwtCheck, jwtParse } from '../middleware/auth';
+import { validateUserRequest } from '../middleware/validation';
 
 const router = express.Router();
 
 // /api/v1/user
 router.post("/", jwtCheck, UserController.createCurrentUser);
+router.put("/", jwtCheck, jwtParse, validateUserRequest, UserController.updateCurrentUser);
 
 export default router;
