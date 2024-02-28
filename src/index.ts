6dcd59447cfc255643806547a0717b4b263aedf3
Taduynguyen <taduynguyen11@gmail.com>
2024-02-28 09:13:10 +0700

    tsconfig

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,17 @@ import express, {Request, Response} from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import mongoose from 'mongoose';
-import UserRoute from '../src/Routes/UserRoute'
+import UserRoute from './routes/UserRoute'
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("Connected to database!"));
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use("/health", async (req: Request, res: Response) => {
+    res.send({message: "Health is OK!"});
+})
 
 // /api/v1/user
 app.use("/api/v1/user", UserRoute)
