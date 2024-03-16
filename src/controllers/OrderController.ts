c5c0523dc20ab00e6be8edec4a71588777aae305
Taduynguyen <taduynguyen11@gmail.com>
2024-03-16 09:03:56 +0700

    Stripe

--- src/controllers/OrderController.ts
+++ src/controllers/OrderController.ts
@@ -1,9 +1,11 @@
 import Stripe from "stripe";
 import { Request, Response } from "express";
 import Restaurant, { MenuItemType } from "../models/restaurant";
+import Order from "../models/order";
 
 const STRIPE = new Stripe(process.env.STRIPE_API_KEY as string);
 const FRONTEND_URL = process.env.FRONTEND_URL as string;
+const STRIPE_ENDPOINT_SECRET = process.env.STRIPE_WEBHOOK_SECRET as string;
 
 type CheckoutSessionRequest = {
   cartItems: {
@@ -20,6 +22,37 @@ type CheckoutSessionRequest = {
   restaurantId: string;
 };
 
+const stripeWebhookHandler = async (req: Request, res: Response) => {
+  let event;
+
+  try {
+    const sig = req.headers["stripe-signature"] as string;
+    event = STRIPE.webhooks.constructEvent(
+      req.body,
+      sig as string,
+      STRIPE_ENDPOINT_SECRET
+    );
+  } catch (error: any) {
+    console.log(error);
+    return res.status(400).send(`Webhook error: ${error.message}`);
+  }
+
+  if (event.type === "checkout.session.completed") {
+    const order = await Order.findById(event.data.object.metadata?.orderId);
+
+    if (!order) {
+      return res.status(404).json({ message: "Order not found" });
+    }
+
+    order.totalAmount = event.data.object.amount_total;
+    order.status = "paid";
+
+    await order.save();
+  }
+
+  res.status(200).send();
+};
+
 const createCheckoutSession = async (req: Request, res: Response) => {
   try {
     const checkoutSessionRequest: CheckoutSessionRequest = req.body;
@@ -30,6 +63,15 @@ const createCheckoutSession = async (req: Request, res: Response) => {
 
     if (!restaurant) throw new Error("Restaurant not found");
 
+    const newOrder = new Order({
+      restaurant: restaurant,
+      user: req.userId,
+      status: "placed",
+      deliveryDetails: checkoutSessionRequest.deliveryDetails,
+      cartItems: checkoutSessionRequest.cartItems,
+      createdAt: new Date(),
+    });
+
     const lineItems = createLineItems(
       checkoutSessionRequest,
       restaurant.menuItems
@@ -37,7 +79,7 @@ const createCheckoutSession = async (req: Request, res: Response) => {
 
     const session = await createSession(
       lineItems,
-      "TEST_ORDER_ID",
+      newOrder._id.toString(),
       restaurant.deliveryPrice,
       restaurant._id.toString()
     );
@@ -48,6 +90,7 @@ const createCheckoutSession = async (req: Request, res: Response) => {
       });
     }
 
+    await newOrder.save();
     res.json({ url: session.url });
   } catch (error: any) {
     console.log(error);
@@ -113,6 +156,7 @@ const createSession = async (
     },
     success_url: `${FRONTEND_URL}/order-status?success=true`,
     cancel_url: `${FRONTEND_URL}/detail/${restaurantId}?cancelled=true`,
+    locale: "vi",
   });
 
   return sessionData;
@@ -120,4 +164,5 @@ const createSession = async (
 
 export default {
   createCheckoutSession,
+  stripeWebhookHandler,
 };
--- src/index.ts
+++ src/index.ts
@@ -12,14 +12,18 @@ mongoose
   .then(() => console.log("Connected to database!"));
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
-app.use(express.json());
+
 app.use(cors());
+
+app.use("/api/v1/order/checkout/webhook", express.raw({ type: "*/*" }));
+
+app.use(express.json());
 
 app.use("/health", async (req: Request, res: Response) => {
   res.send({ message: "Health is OK!" });
@@ -28,7 +32,7 @@ app.use("/health", async (req: Request, res: Response) => {
 app.use("/api/v1/user", UserRoute);
 app.use("/api/v1/restaurant", RestaurantRoute);
 app.use("/api/v1/other/restaurant", OtherRestaurantRoute);
-app.use("/api/v1/orther", OrderRoute)
+app.use("/api/v1/order", OrderRoute);
 
 app.listen(7000, () => {
   console.log("Server started on localhost:7000");
